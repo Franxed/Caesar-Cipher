4939e63709268dd961aba45b7dfadf3211fd2138
Franx <fe46315@gmail.com>
2024-09-20 10:17:04 +0200

    Started Project

---
+++ cipher.py
@@ -0,0 +1,17 @@
+# Importing libraries.
+import string
+
+# Print alphabetic characters.
+alphabet = list(string.ascii_uppercase)
+print(alphabet)     # Testing.
+
+
+# Function to encrypt a message.
+def encrypt():
+    pass
+
+
+# Function to decrypt a message.
+def decrypt():
+    pass
+
